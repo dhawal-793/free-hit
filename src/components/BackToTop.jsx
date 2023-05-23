addb81c2dcd5da1d53dec199ec7ba2eada3177ed
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-23 13:18:40 +0530

    Updated BackToTop Button to use TailwindCSS

--- src/components/BackToTop.jsx
+++ src/components/BackToTop.jsx
@@ -1,32 +1,32 @@
-import React, { useEffect, useState } from 'react'
+'use client'
+
+import { useEffect, useState } from 'react'
 import { Link } from 'react-scroll'
+import { RiArrowUpSLine } from "react-icons/ri"
 
 const BackToTopButton = () => {
-  const [btnVisiblity, setBtnVisiblity] = useState(false)
-  const handleScroll = () => {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
-  }
-  useEffect(() => {
-    const toggleVisiblity = () => {
-      window.pageYOffset > 250 ? setBtnVisiblity(true) : setBtnVisiblity(false)
+    const [btnVisiblity, setBtnVisiblity] = useState(false)
+    const handleScroll = () => {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
     }
-    window.addEventListener('scroll', toggleVisiblity)
-    return () => {
-      window.removeEventListener('scroll', toggleVisiblity)
-    }
-  }, [])
+    useEffect(() => {
+        const toggleVisiblity = () => {
+            window.pageYOffset > 250 ? setBtnVisiblity(true) : setBtnVisiblity(false);
+        }
+        window.addEventListener('scroll', toggleVisiblity);
+        return () => {
+            window.removeEventListener('scroll', toggleVisiblity);
+        }
+    }, [])
 
-  return (
-    <Link onClick={handleScroll} to="Top" smooth duration={500}>
-      <div
-        className={` ${
-          !btnVisiblity ? 'transition-before' : 'transition-after'
-        } backToTopButton`}
-      >
-        <i className="ri-arrow-up-s-line"></i>
-      </div>
-    </Link>
-  )
+
+    return (
+        <Link onClick={handleScroll} to="Top" smooth duration={500}>
+            <div className={` ${!btnVisiblity ? "-translate-y-[100vh]" : "translate-y-0"} fixed z-50 bottom-2 right-2 transition-all delay-100 duration-500 w-10 h-10 rounded-full cursor-pointer flex justify-center items-center text-light-primary bg-dark-primary text-2xl group`}>
+                <RiArrowUpSLine className='group-hover:scale-125 duration-500 transition-all' />
+            </div>
+        </Link>
+    )
 }
 
-export default BackToTopButton
+export default BackToTopButton;
