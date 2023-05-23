ecd48952e6a0b7dab9a5a7bcee46be1353f084d0
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-23 13:06:12 +0530

    Updated Layout to include Navbar inside body

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,6 +1,6 @@
 
 import './globals.css'
-
+import Navbar from '../components/Navbar'
 
 export const metadata = {
     title: 'Create Next App',
@@ -11,6 +11,7 @@ export default function RootLayout({ children }) {
     return (
         <html lang="en">
             <body >
+                <Navbar />
                 {children}
             </body>
         </html>
