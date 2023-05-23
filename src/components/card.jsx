ce946596b1f3e397f8bf42a9851184477660f402
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-23 18:59:44 +0530

    Created Card component with appropriate styles

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,70 +1,26 @@
-import Header from './header'
-import React, { useContext } from 'react'
-import { ToolContext } from '../App'
 
-const Card = ({ length }) => {
-  const {
-    filteredProducts,
-    category,
-    handelBookmarkAdd,
-    bookmarkfilteredProducts,
-    deleteres,
-  } = useContext(ToolContext)
+const Card = ({ productData: { image, productName, description, link } }) => {
 
-  return (
-    <>
-      <Header />
-      <div className="card-container">
-        {length == 0 ? (
-          <p className="no-results">
-            Sorry, no tools available for this search term
-          </p>
-        ) : (
-          <main className="grid">
-            {filteredProducts.map((product, index) => {
-              return category === 'all' || category === product.category ? (
-                <article key={index}>
-                  <div className="text">
-                    <h3>
-                      <img className="card-img" src={product.image} alt="" />
-                      <span className="card-title">{product.productName}</span>
-                    </h3>
-                    <p>{product.description}</p>
-                    <div className="btn-cont">
-                      <a target="_blank" href={product.link}>
-                        <button className='visit'>
-                          <font color="white" size="4">
-                            Visit
-                          </font>
-                        </button>
-                      </a>
-                      {bookmarkfilteredProducts.some(
-                        (obj) => obj['productName'] === product.productName
-                      ) ? (
-                        <button onClick={() => deleteres(product)}>
-                          <a href="#">
-                            Delete<i className="ri-bookmark-fill"></i>
-                          </a>
-                        </button>
-                      ) : (
-                        <a href="#">
-                          <button className='bookmark' onClick={() => handelBookmarkAdd(product)}>
-                            <font color="white" size="4">
-                              Bookmark
-                            </font>
-                          </button>
-                        </a>
-                      )}
-                    </div>
-                  </div>
-                </article>
-              ) : null
-            })}
-          </main>
-        )}
-      </div>
-    </>
-  )
+    return (
+        <article className=" relative h-56 max-w-[350px] border border-dark-primary shadow-card bg-light-primary py-2.5 rounded-3xl hover:shadow-card-hover flex flex-col justify-between">
+            <div className="px-4 mb-3">
+                <div className="flex gap-2 items-center">
+                    <img src={image} className="w-10 h-10 rounded-xl" alt={productName} />
+                    <h3 className="w-full text-start text-2xl font-bold">{productName}</h3>
+                </div>
+                <div className="mt-3 font-semibold">
+                    {description}
+                </div>
+            </div>
+            <div className="pb-3">
+                <a href={link} target="_blank" className="capitalize bg-dark-primary text-light-primary py-2 px-6 rounded-lg m-0.5 cursor-pointer hover:scale-105 inline-block transition-all">Visit
+                </a>
+                <button className="capitalize bg-dark-primary text-light-primary py-2 px-6 rounded-lg m-0.5 cursor-pointer hover:scale-105 transition-all">
+                    Bookmark
+                </button>
+            </div>
+        </article>
+    )
 }
 
 export default Card
