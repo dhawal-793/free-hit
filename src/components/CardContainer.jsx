48b80f82dde32bb6d8a3476e22eda1a3468fafcc
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-05-26 17:44:56 +0530

    Updated Card component

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,5 +1,6 @@
 import Card from "./Card"
 
+
 const CardContainer = ({ productsData }) => {
   return (
     <div className="p-5 w-full grid grid-view justify-center items-start gap-4 mt-4 min-h-[55vh] text-center">
