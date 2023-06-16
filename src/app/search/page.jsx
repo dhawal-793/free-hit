06ba5222bc8298a7a93398e0fd4397015a9c73ca
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-16 22:18:19 +0530

    Updated Search route to display dynamic data

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,6 +1,18 @@
-const Searchpage = () => {
+import CardContainer from "@/components/CardContainer"
+import products from "@/DB/product.json"
+import { notFound } from "next/navigation"
+
+
+const Searchpage = ({ searchParams }) => {
+
+  const data = products.filter(
+    (el) => el.productName.toLowerCase().includes(searchParams.term)
+  )
+
+  if (data.lengt < 1) return notFound()
+
   return (
-    <div>Searchpage</div>
+    <CardContainer productsData={data} />
   )
 }
 
