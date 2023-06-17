f696ee2729a93a175bdc0ce72fec1e89e81be33b
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-17 16:00:27 +0530

    Aadded button states, function & Button component

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,22 +1,39 @@
-const Card = ({ productData: { image, productName, description, link } }) => {
+'use client'
+
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { useBookmarkContext } from "@/context/bookmarkContext";
+import Button from "@/components/card/Button";
+
+const Card = ({ productData }) => {
+    const { description, image, link, productName } = productData;
+    const { bookmarks, addBookmark, removeBookmark } = useBookmarkContext()
+    const path = usePathname()
+
+    useEffect(() => { }, [bookmarks])
 
     return (
-        <article className=" relative h-56 max-w-[350px] border border-dark-primary shadow-card bg-light-primary py-2.5 rounded-3xl hover:shadow-card-hover flex flex-col justify-between">
-            <div className="px-4 mb-3">
-                <div className="flex gap-2 items-center">
-                    <img src={image} className="w-10 h-10 rounded-xl" alt={productName} />
-                    <h3 className="w-full text-start text-2xl font-bold">{productName}</h3>
+        <article className=" relative mx-auto h-56 max-w-[300px] sm:min-w-[350px] md:max-w-sm border border-dark-primary shadow-card bg-light-primary py-2.5 rounded-3xl hover:shadow-card-hover flex flex-col justify-between">
+            <div className="w-full px-2 mb-3 sm:px-4">
+                <div className="flex items-center gap-2">
+                    <img src={image} className="rounded-lg w-7 md:w-10 h-7 md:h-10 md:rounded-xl" alt={productName} />
+                    <h3 className="w-full text-lg font-semibold text-start md:text-2xl md:font-bold">{productName}</h3>
                 </div>
-                <div className="mt-3 font-semibold">
+                <div className="mt-3 font-normal md:font-semibold">
                     {description}
                 </div>
             </div>
-            <div className="pb-3">
-                <a href={link} target="_blank" className="capitalize bg-dark-primary text-light-primary py-2 px-6 rounded-lg m-0.5 cursor-pointer hover:scale-105 inline-block transition-all">Visit
+            <div className="flex pb-3 mx-auto space-x-3 w-fit">
+                <a href={link} target="_blank" className="capitalize bg-dark-primary text-light-primary py-2 px-3 sm:px-6 rounded-lg m-0.5 cursor-pointer hover:scale-105 inline-block transition-all">
+                    Visit
                 </a>
-                <button className="capitalize bg-dark-primary text-light-primary py-2 px-6 rounded-lg m-0.5 cursor-pointer hover:scale-105 transition-all">
-                    Bookmark
-                </button>
+
+                {path === "/bookmarks" ? <Button handleClick={() => removeBookmark(productData)} text="Remove" /> :
+                    bookmarks.some((obj) => obj['productName'] === productName) ?
+                        <Button text="Remove" handleClick={() => removeBookmark(productData)} />
+                        :
+                        <Button text="Bookmark" handleClick={() => addBookmark(productData)} />
+                }
             </div>
         </article>
     )
