13e0485c98bc0a199b4e3117644feaf036fdf251
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-17 11:09:20 +0530

    Updated import to for Navbar component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,5 +1,5 @@
 import './globals.css'
-import Navbar from '@/components/Navbar'
+import Navbar from '@/components/navigation/Navbar'
 import BackToTopButton from '@/components/BackToTop'
 
 export const metadata = {
