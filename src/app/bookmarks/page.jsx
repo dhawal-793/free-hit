46664c616eb16fad83a97317ea5deaab51bfb600
Dhawal Vijayvargiya <88284372+dhawal-793@users.noreply.github.com>
2023-06-17 11:15:00 +0530

    Updatde bookmarks page to use bookmarkContext

--- src/app/bookmarks/page.jsx
+++ src/app/bookmarks/page.jsx
@@ -1,7 +1,34 @@
+'use client'
+
+import CardContainer from "@/components/CardContainer"
+import { useEffect, useState } from "react"
+import { useBookmarkContext } from "@/context/bookmarkContext"
 
 const Bookmarks = () => {
+  const { bookmarks } = useBookmarkContext()
+  const [bookmarksLoading, setBookmarksLoading] = useState(true)
+
+  useEffect(() => {
+    setBookmarksLoading(false)
+  }, [])
+
+  useEffect(() => { }, [bookmarks])
+
   return (
-    <div>Bookmarks</div>
+    <>
+      {
+
+        bookmarksLoading ? <p>Loading...</p>
+          :
+          bookmarks.length ?
+            <CardContainer productsData={bookmarks} />
+            :
+            <p className="text-xl font-semibold text-center">
+              Sorry, no BookMarks to Show
+            </p>
+
+      }
+    </>
   )
 }
 
